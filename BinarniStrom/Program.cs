ae1db6739c4add4cc0400d159b18c511380fbb17
Dominik Šefr <dominik.sefr@pslib.cz>
2019-12-18 09:23:30 +0100

    Prvni Commit

---
+++ BinarniStrom/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace BinarniStrom
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int,int> slovnik = new MyDictionary<int,int>();
+            slovnik.Add(2, 2);
+            slovnik.Add(1, 1);
+            slovnik.Add(3, 3);
+            slovnik.Add(9, 9);
+            slovnik.Add(5, 5);
+            slovnik.Add(8, 8);
+            slovnik.Add(6, 6);            
+            Console.WriteLine(slovnik);
+            Console.WriteLine("Pocet: " + slovnik.Count);
+            Console.WriteLine("Max: " + slovnik.Max);
+            Console.WriteLine("Min: " + slovnik.Min);
+        }
+    }
+}
